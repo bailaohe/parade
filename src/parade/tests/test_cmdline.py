379d8c9225036d60baf70437452b8b9db6c69409
bailaohe <bailaohe@gmail.com>
2017-09-22 13:46:07 +0800

    add pytest

--- src/parade/tests/test_cmdline.py
+++ src/parade/tests/test_cmdline.py
@@ -1,7 +1,6 @@
-from parade.cmdline import execute
+from ..cmdline import execute
+
+
 class TestCmdline(object):
     def test_no_cmd(self):
         assert execute() == 0
-
-    def test_one(self):
-        assert 1 == 1
